ca7c32124debf3d26319224087a45e3c1dd682dd
Alexey Tsvetkov <tsvetkov.frost@gmail.com>
2025-12-04 14:54:02 +1000

    create user managerdependency

--- fastapi-application/api/dependencies/user_manager.py
+++ fastapi-application/api/dependencies/user_manager.py
@@ -9,10 +9,11 @@ from .users import get_users_db
 if TYPE_CHECKING:
     from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 
+
 async def get_user_manager(
-        user_db: Annotated[
-            "SQLAlchemyUserDatabase",
-            Depends(get_users_db),
-        ]
+    user_db: Annotated[
+        "SQLAlchemyUserDatabase",
+        Depends(get_users_db),
+    ],
 ):
     yield UserManager(user_db)
