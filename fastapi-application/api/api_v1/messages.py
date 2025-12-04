86ff16760b7ae3030f2616746d2c0d6db20f73d4
Alexey Tsvetkov <tsvetkov.frost@gmail.com>
2025-12-04 19:18:35 +1000

    create views for authenticated users and superusers

--- fastapi-application/api/api_v1/messages.py
+++ fastapi-application/api/api_v1/messages.py
@@ -1,8 +1,40 @@
-from fastapi import APIRouter
+from typing import Annotated
 
+from fastapi import APIRouter
+from fastapi import Depends
+
+from api.api_v1.fastapi_users import current_active_user, current_active_super_user
 from core.config import settings
+from core.models import User
+from core.schemas.user import UserRead
 
 router = APIRouter(
     prefix=settings.api.v1.messages,
     tags=["Messages"],
 )
+
+
+@router.get("")
+def get_user_messages(
+    user: Annotated[
+        User,
+        Depends(current_active_user),
+    ],
+):
+    return {
+        "message": ["m1", "m2", "m3"],
+        "user": UserRead.model_validate(user),
+    }
+
+
+@router.get("/secrets")
+def get_superuser_messages(
+    user: Annotated[
+        User,
+        Depends(current_active_super_user),
+    ],
+):
+    return {
+        "message": ["secret-m1", "secret-m2", "secret-m3"],
+        "user": UserRead.model_validate(user),
+    }
